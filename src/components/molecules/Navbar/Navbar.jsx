ed15b97800f5f4664f1d358fa3e624dc7f1e1ef1
marugandev <marugandev@gmail.com>
2025-05-15 01:15:33 +0200

    UPDATE Link active

--- src/components/molecules/Navbar/Navbar.jsx
+++ src/components/molecules/Navbar/Navbar.jsx
@@ -1,21 +1,46 @@
 import "./Navbar.css";
 
-import { NavLink } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { navRoutes } from "../../../routes/navRoutes";
+import { useQueryParams } from "../../../hooks/useQueryParams";
 
 const Navbar = () => {
+  const location = useLocation();
+  const { category, subcategory } = useQueryParams();
+
+  const isRouteActive = (route) => {
+    const routePath = route.path;
+    const [routeBase, routeSearch] = routePath.split("?");
+    const locationBase = location.pathname;
+
+    if (!routeSearch) {
+      return locationBase === routeBase;
+    }
+
+    const routeParams = new URLSearchParams(routeSearch);
+    const routeCategory = routeParams.get("category");
+    const routeSubcategory = routeParams.get("subcategory");
+
+    const matchCategory = routeCategory ? routeCategory === category : true;
+    const matchSubcategory = routeSubcategory
+      ? routeSubcategory === subcategory
+      : true;
+
+    return locationBase === routeBase && matchCategory && matchSubcategory;
+  };
+
   return (
     <nav className="navbar">
       <ul>
-        {navRoutes?.map((route) => (
+        {navRoutes.map((route) => (
           <li className="list-item" key={route.id}>
-            <NavLink
+            <Link
               to={route.path}
-              className={({ isActive }) => `link ${isActive ? "active" : ""}`}
+              className={`link ${isRouteActive(route) ? "active" : ""}`}
               style={{ marginLeft: `${route.level * 1.5}rem` }}
             >
               {route.title}
-            </NavLink>
+            </Link>
           </li>
         ))}
       </ul>
