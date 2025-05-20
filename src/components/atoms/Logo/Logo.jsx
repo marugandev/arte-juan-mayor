bb41fcc7ff636cee1721da91509fb27747087658
marugandev <marugandev@gmail.com>
2025-05-20 13:39:41 +0200

    Update local Logo images

--- src/components/atoms/Logo/Logo.jsx
+++ src/components/atoms/Logo/Logo.jsx
@@ -4,7 +4,7 @@ const Logo = () => {
   return (
     <img
       className="logo"
-      src="https://res.cloudinary.com/cloudcloudinary0/image/upload/v1746031447/arte_juan_mayor/tonyg4luvm7jngbzltur.svg"
+      src="/images/logos/logo_juan_mayor.svg"
       alt="Logo Arte Juan Mayor"
     />
   );
